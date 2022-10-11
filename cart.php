267bd49d4ec9894259ce09ed4ce603fdedaccf18
AlexC2103 <80043678+AlexC2103@users.noreply.github.com>
2022-10-12 02:06:04 +0300

    acasă

--- cart.php
+++ cart.php
@@ -18,12 +18,13 @@
     echo $localStorage;
 
     $dataReceived = json_decode($localStorage, true);
-    var_dump($dataReceived);
+    var_dump($dataReceived["productList"]["value"]);
+    echo $dataReceived["productList"]["value"][1][0];
 
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $idComanda = $_POST["orderId"];
+        $idToFind = $_POST["orderId"];
 
         $comanda = $conn->prepare("INSERT INTO comanda (livrare_contact, livrare_adresa, livrare_telefon, livrare_email, total, transport)
                                          VALUES (:contact, :adresa, :telefon, :email, :total, :transport)");
@@ -36,15 +37,32 @@
         $comanda->bindParam(':transport', $transport);
 
         $contact = $dataReceived["facturare"]["numeCumparator"];
-        $adresa = $dataReceived["livrare"]["adresa"]["strada"];
+        $adresa = $dataReceived["livrare"]["adresa"]["strada"]; $adresa .= ", ";
+        $adresa .= $dataReceived["livrare"]["adresa"]["oras"]; $adresa .= ", ";
+        $adresa .= $dataReceived["livrare"]["adresa"]["judet"];
         $telefon = $dataReceived["facturare"]["phoneNumber"];
         $email = $dataReceived["facturare"]["email"];
         $total = 1500;
         $transport = 25;
-        //$comanda->execute();
+        $comanda->execute();
 
-        $produse = $conn->prepare("SELECT nume_produs, cantitate_produs, total FROM produse WHERE id_comanda=$idComanda;");
-        $comanda = $conn->prepare("SELECT * FROM comanda WHERE id=$idComanda");
+        //Caz ipotetic: ce se intampla in cazul in care 2 oameni dau comanda in acelasi timp?
+
+        $orderId = $conn->lastInsertId();
+
+        foreach ($dataReceived["productList"]["value"] as $k) {
+            $produse = $conn->prepare("INSERT INTO produse (nume_produs, cantitate_produs, id_comanda)
+                                             VALUES (:numeProdus, :cantitateProdus, :idComanda)");
+
+            $produse->bindParam(':numeProdus', $k[0]);
+            $produse->bindParam(':cantitateProdus', $k[1]);
+            $produse->bindParam(':idComanda', $orderId);
+            $produse->execute();
+            echo $k[0], ' ';
+        }
+
+        $produse = $conn->prepare("SELECT nume_produs, cantitate_produs, total FROM produse WHERE id_comanda=$idToFind;");
+        $comanda = $conn->prepare("SELECT * FROM comanda WHERE id=$idToFind");
         $produse->execute();
         $comanda->execute();
 
